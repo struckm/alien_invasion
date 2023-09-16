c42ae6aa8eab734b762dc1414f20fe24f6114faf
Mark Struck <markstruck@comcast.net>
2023-09-15 19:36:33 -0500

    added new movement to the ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -8,9 +8,7 @@ class AlienInvasion:
 
     def __init__(self):
         """Initialize the game, and create game resources."""
-
         pygame.init()
-
         self.clock = pygame.time.Clock()
         self.settings = Settings()
         
@@ -21,12 +19,11 @@ class AlienInvasion:
 
         self.ship = Ship(self)
 
-
     def run_game(self):
         """Start the main loop for the game."""
-
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             self.clock.tick(60)
 
@@ -36,9 +33,25 @@ class AlienInvasion:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.type == pygame.K_RIGHT:
-                    # Move the ship to the right.
-                    self.ship.rect.x += 1
+                self._check_keydown_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = True
+        elif event.key == pygame.K_q:
+            sys.exit()
+
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.ship.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.ship.moving_left = False
 
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
